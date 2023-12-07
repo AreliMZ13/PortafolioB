cdf10df9539ad3df78d1ba26e129f5700506d16c
Areli <areli.moreno213@gmail.com>
2023-12-06 20:54:39 -0600

    feat:carrusel-porafolio

---
+++ script.js
@@ -0,0 +1,55 @@
+
+
+//Función que me aplica el estilo a la opciÓn seleccionada y quita la previamente seleccionada
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    opciones[5].className = "";
+    link.className = "seleccionado";
+
+    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
+    //en modo responsive
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+//función que muestra el menu responsive
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
+
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("html");
+        habilidades[1].classList.add("css");
+        habilidades[2].classList.add("bootstrap");
+        habilidades[3].classList.add("javascrip");
+        habilidades[4].classList.add("java");
+        habilidades[5].classList.add("figma");
+        habilidades[6].classList.add("trello");
+        habilidades[7].classList.add("jira");
+        habilidades[8].classList.add("scrum");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
+
+//Carrusel
